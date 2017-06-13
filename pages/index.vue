bb3c60c95b03e711b5971788920c62d85c258f87
chriswburton <nuclearcornflakes@gmail.com>
2017-06-13 12:49:21 +0100

    Expanded our template view + added custom filter

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,37 @@
 <template>
   <section class="container">
     <h1>My Team</h1>
-    <ul>
-      <li v-for="user in users">{{user.firstname}} {{user.lastname}}</li>
-    </ul>
+    <table>
+      <thead>
+        <th>Employee Name</th>
+        <th>Employee Role(s)</th>
+        <th>This user manages:</th>        
+      </thead>
+      <tbody>
+        <tr v-for="user in users">
+          <td>{{user.firstname}} {{user.lastname}}</td>
+          <td>
+            <template v-if="user.roleIds && user.roleIds.length > 0">{{user.roleIds | findById(roles)}}</template>
+            <template v-else>This user does not have a role</template>
+          </td>          
+          <td>
+            <span v-if="user.managerOf && user.managerOf.length > 0">{{user.managerOf | findById(users)}}</span>
+            <span v-else>This user does not manage any staff</span>
+          </td>       
+        </tr>
+      </tbody>
+    </table>
     <h2>Add a new team member</h2>
     <form @submit.prevent="addUser({firstname, lastname}); resetFields()">
-      <input type="text" placeholder="Enter their firstname" v-model="firstname">
-      <input type="text" placeholder="Enter their lastname" v-model="lastname">
-      <button role="button">Submit</button>
+      <input type="text" placeholder="Enter their firstname" v-model="firstname" v-bind:class="{invalid: firstname.length===0}">
+      <input type="text" placeholder="Enter their lastname" v-model="lastname" v-bind:class="{invalid: lastname.length===0}">
+      <button role="button" v-bind:disabled="firstname.length===0 || lastname.length===0">Submit</button>
     </form> 
   </section>
 </template>
+<style scoped>
+  .invalid { border: 2px solid red }
+</style>
 <script>
   // get our required helpers from vuex
   import { mapState, mapActions } from 'vuex'
@@ -23,6 +43,14 @@
         firstname: '',
         lastname: ''
       }
+    },
+    // define filters to help us
+    filters: {
+      findById: (chosen, all) => all.filter(
+        item => chosen.includes(item.id)
+      ).map(
+        item => item.firstname ? item.firstname + ' ' + item.lastname : item.title
+      ).join(', ')
     },
     // spread the necessary props from our vuex state into this component
     computed: {
