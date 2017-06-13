96df92931bd9942d4f6fb4a0e45962339abfd238
chriswburton <nuclearcornflakes@gmail.com>
2017-06-13 10:15:59 +0100

    Added default state with temporary mock data

---
+++ store/index.js
@@ -0,0 +1,71 @@
+//  declare a mock initial state for our app
+export const state = () => {
+  return {
+    roles: [
+      {
+        id: 0,
+        title: 'Software Developer'
+      },
+      {
+        id: 1,
+        title: 'Business Development Manager'
+      },
+      {
+        id: 2,
+        title: 'Marketing Manager'
+      },
+      {
+        id: 3,
+        title: 'Help and Support'
+      }
+    ],
+    users: [
+      {
+        id: 0,
+        firstname: 'Chris',
+        lastname: 'Burton',
+        roleIds: [0],
+        managerOf: [1, 2],
+        permissions: {
+          admin: true
+        }
+      },
+      {
+        id: 1,
+        firstname: 'Michael',
+        lastname: 'Statham',
+        roleIds: [0],
+        permissions: {
+          admin: false
+        }
+      },
+      {
+        id: 2,
+        firstname: 'Hayley',
+        lastname: 'Puplett',
+        roleIds: [2, 3],
+        permissions: {
+          admin: false
+        }
+      },
+      {
+        id: 3,
+        firstname: 'David',
+        lastname: 'Brown',
+        roleIds: [3],
+        permissions: {
+          admin: false
+        }
+      },
+      {
+        id: 4,
+        firstname: 'Jack',
+        lastname: 'Tango',
+        roleIds: [1],
+        permissions: {
+          admin: true
+        }
+      }
+    ]
+  }
+}
